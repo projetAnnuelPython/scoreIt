625e05524ecc921ce83f4ac200d2652f38b7068a
fodeguipro@gmail.com <fodeguipro@gmail.com>
2017-06-18 00:07:26 +0200

    home screeen

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tool.JsonParser import JsonParser
 from database.databaseConnection import SqlDbConnection
-
+from view.App import *
+import tkinter as tk
+import tkinter
+#   self.root.bind('<FocusIn>', self.focus_in)
 # load json resources
 json_parser = JsonParser()
 settings = json_parser.parsefile('settings')
 
 sqlConnect = SqlDbConnection(settings['sql']['login'], settings['sql']['password'],
                              settings['sql']['database_name'])
-
 sqlConnect.get_connection()
+
+root = tk.Tk()
+root.geometry('500x300+300+40')
+root.title('SCORE IT')
+root.resizable(width=False, height=False)
+
+
+app = App(root)
+app.pack(side="top", fill="both", expand=True)
+root.mainloop()
+
---
+++ view/App.py
@@ -0,0 +1,66 @@
+import tkinter as tk;
+from tkinter import *
+import os,sys
+
+
+LARGE_FONT = ("Verdana", 22)
+
+
+class App(tk.Frame):
+
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.Frames = {}
+        self.create_window()
+
+    def create_window(self):
+        self.home = Home(self)
+        self.Frames['current'] = self.home
+        self.home.pack(side="top")
+        self.home.tkraise()
+
+    def show_frame(self):
+        self.Frames['current'].destroy()
+        user_view = user_profile(self)
+        self.Frames['current']
+        user_view.pack(side="top")
+        user_view.tkraise()
+
+
+class Home(tk.Frame):
+    def __init__(self, controller):
+        tk.Frame.__init__(self, controller)
+        self.grid(row=0, column=0, sticky="nsew")
+        self.controller = controller
+
+        login_label = tk.Label(self, text="Login", font=LARGE_FONT)
+        login_label.pack(pady=10, padx=10)
+
+        self.login = StringVar()
+        login_entry = tk.Entry(self, textvariable=self.login)
+        login_entry.pack(pady=10, padx=10)
+
+        password_label = tk.Label(self, text="Mot de passe", font=LARGE_FONT)
+        password_label.pack(pady=10, padx=10)
+
+        self.password = StringVar()
+        password_entry = tk.Entry(self, textvariable=self.password)
+        password_entry.pack()
+
+        #button_image = image.open(file="/Users/fofofofodev/Desktop/ESGI3A/OUTILS DU DEVELOPPEUR /ProjetAnnuel/scoreIt/view/go.gif")
+        validate_button = Button(self, height=100, text="VALIDER", command=lambda: controller.show_frame(),
+                                 relief="ridge", bg="#000fff000")
+        #validate_button.config(font=("Verdana", 20))
+        validate_button.pack(fill=BOTH)
+
+
+class user_profile(tk.Frame):
+    def __init__(self, controller):
+        tk.Frame.__init__(self, controller)
+        self.controller = controller
+        #back_button = tk.Button(self, text="RETOUR", width=10, command=controller.create_window())
+        #back_button.pack()
+        label = tk.Label(self, text="USER PROFILE!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
